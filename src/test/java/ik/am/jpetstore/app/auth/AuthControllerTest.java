285fdf9fc2bd518052575d85ff0af1c597d9c7f6
gavin2lee <gavin2lee@163.com>
2016-09-07 09:26:33 +0800

    add test

---
+++ src/test/java/ik/am/jpetstore/app/auth/AuthControllerTest.java
@@ -0,0 +1,19 @@
+package ik.am.jpetstore.app.auth;
+
+//import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AuthControllerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetUserAccount() {
+		//fail("Not yet implemented");
+	}
+
+}
