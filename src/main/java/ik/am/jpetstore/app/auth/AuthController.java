5565d64408f308c714aa085227c303a4296de9d1
gavin2lee <gavin2lee@163.com>
2016-09-07 17:23:31 +0800

    spring request

--- src/main/java/ik/am/jpetstore/app/auth/AuthController.java
+++ src/main/java/ik/am/jpetstore/app/auth/AuthController.java
@@ -13,19 +13,64 @@ import ik.am.jpetstore.domain.model.Account;
 @RequestMapping("auth")
 public class AuthController {
 	private static final Logger log = LoggerFactory.getLogger(AuthController.class);
-	
-	@RequestMapping(value="account",produces="application/json")
+
+	@RequestMapping(value = "account", produces = "application/json")
 	@ResponseBody
-	public Account getUserAccount(@RequestBody AuthForm af){
+	public Account getUserAccount(@RequestBody AuthForm af) {
 		log.debug(String.format("%s", af));
-		
+
 		Account a = new Account();
 		a.setUsername(af.getUsername());
 		a.setPassword(af.getPassword());
-		
+
 		a.setCountry("China");
 		a.setCity("深圳");
-		
+
+		return a;
+	}
+
+	@RequestMapping(value = "account1", produces = "application/json")
+	@ResponseBody
+	public Account getUserAccountByParams(String username, String password) {
+		log.debug(String.format("%s - %s", username, password));
+
+		Account a = new Account();
+		a.setUsername(username);
+		a.setPassword(password);
+
+		a.setCountry("China");
+		a.setCity("上海");
+
+		return a;
+	}
+	
+	@RequestMapping(value = "account2", produces = "application/json")
+	@ResponseBody
+	public Account getUserAccountByParamsForm(AuthForm af) {
+		log.debug(String.format("%s", af));
+
+		Account a = new Account();
+		a.setUsername(af.getUsername());
+		a.setPassword(af.getPassword());
+
+		a.setCountry("China");
+		a.setCity("北京");
+
+		return a;
+	}
+	
+	@RequestMapping(value = "account3", produces = "application/json",consumes="application/json")
+	@ResponseBody
+	public Account getUserAccountByParamsJson(AuthForm af) {
+		log.debug(String.format("%s", af));
+
+		Account a = new Account();
+		a.setUsername(af.getUsername());
+		a.setPassword(af.getPassword());
+
+		a.setCountry("China");
+		a.setCity("南京");
+
 		return a;
 	}
 }
