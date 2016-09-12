c2a908b51132dff0d289f5bca1583d41f64f15c0
59763227 <59763227@qq.com>
2016-09-13 00:05:52 +0800

    cxf

--- src/main/java/com/gl/petstore/pm/ProjectManager.java
+++ src/main/java/com/gl/petstore/pm/ProjectManager.java
@@ -1,6 +1,10 @@
 package com.gl.petstore.pm;
 
+import javax.jws.WebService;
+
+@WebService(name = "ProjectManager", targetNamespace = "http://pm.petstore.gl.com/", wsdlLocation = "http://pm.petstore.gl.com/ProjectManagerService?wsdl")
 public interface ProjectManager {
 	String getProjectName();
+
 	String getProjectNameFromName(String projectName);
 }
